a3ace07b1aec7bc1963ffda4b65d48b7383e9c0f
BilgeWang0304 <bilge04871@gmail.com>
2024-09-18 10:05:44 -0400

    Fixed JsonResponse safe=False error and updated login view

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
-import Login from "./components/Login/Login"
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
     </Routes>
   );
 }
